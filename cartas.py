b4bfc2f10da041eebd6a7865b2dbb0b032cc94c1
Gabriel Wisniewski <wisniewski_gabriel_@outlook.com>
2024-09-12 01:36:09 -0300

    ...

--- cartas.py
+++ cartas.py
@@ -7,6 +7,7 @@ initial_card = 1
 naipe_range = [0, 1, 2, 3]
 final_card = 12.3
 forbidden_range = [8.0, 9.9]
+max_hand_len = 3
 card_count_threshold = 40
 
 
@@ -40,12 +41,12 @@ def make_sure_cards_are_ordinal(cards:list):
 def get_jokers(starter_card: float, cards: list):
 
     index = cards.index(starter_card)
-    future_index = index + len(naipe_range)
+    index = index + len(naipe_range)
 
-    if future_index >= len(cards):
-        future_index -= len(cards)
+    if index >= len(cards):
+        index -= len(cards)
 
-    lower_joker = floor(cards[future_index])
+    lower_joker = floor(cards[index])
     jokers = []
 
     for naipe in naipe_range:
@@ -54,9 +55,29 @@ def get_jokers(starter_card: float, cards: list):
     return jokers
 
 
+# pegar 3 cartas aleatorias, importante passar a list de cartas na mesa
 def get_hand_of_cards(cards: list, taken_cards: list):
 
-    hand_len = 3
+    cards = [item for item in cards if item not in taken_cards]
+    hand = []
+
+    for idx in range(max_hand_len):
+
+        random_idx = random.randint(a=0, b=len(cards))
+        card = cards[random_idx]
+        cards.remove(card)
+        hand.append(card)
+
+    return hand
+
+
+def get_winner_card(cards_in_table: list, cards: list, jokers: list):
+
+    table_has_jokers: bool = len([item for item in jokers if item not in cards_in_table]) > 0
+
+
+print(get_winner_card(cards_in_table=[1.3, 3.1, 12.2, 10.2]))
+
 
 
 
